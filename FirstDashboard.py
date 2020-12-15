4c52c6f19f16f78151e930a143a33977b26b3b3e
Moltomay <hamza.benkhalil25@gmail.com>
2020-12-15 19:16:11 +0100

    movies

--- FirstDashboard.py
+++ FirstDashboard.py
@@ -7,20 +7,29 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 data = pd.read_csv("items.csv")
 data=data.drop([0,1,3,17,18],axis=0)
 data=data.sort_values(by=['product_price'])
 new_data=data[0:5]
 DVDs=new_data.iloc[:,0]
 Prices=new_data.iloc[:,1]
+
+movies = pd.read_csv("movies.csv")
+movies=movies.sort_values(by=['movie_rating'])
+new_data=movies.drop([17,9,16,18,1,15,6,10,7,14],axis=0)
+movies_name=new_data.iloc[:,0]
+movies_rating=new_data.iloc[:,1]
+
+
 @app.route('/')
 def index():
-    feature = 'First plot'
+    feature = 'Amazon plot'
     bar = create_plot(feature)
     return render_template('index.html', plot=bar)
 
 def create_plot(feature):
-    if feature == 'First plot':
+    if feature == 'Amazon plot':
         x = DVDs
         y = Prices
         df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
@@ -39,18 +48,18 @@ def create_plot(feature):
 
 
 
-    if feature=='Second plot':
-        x = DVDs
-        y = Prices
+    elif feature=='IMDB plot':
+        x = movies_name
+        y = movies_rating
         df = pd.DataFrame({'x': x, 'y': y})  # creating a sample dataframe
         data = [
-            go.Scatter(
+            go.Bar(
                 x=df['x'],  # assign x as the dataframe column 'x'
                 y=df['y']
             )
         ]
 
-    if feature == 'Third plot':
+    elif feature == 'Third plot':
         x = Prices
         y = Prices
         df = pd.DataFrame({'x': x, 'y': y})  # creating a sample dataframe
@@ -61,7 +70,7 @@ def create_plot(feature):
                 )
             ]
 
-    if feature == 'Fourth plot':
+    elif feature == 'Fourth plot':
         x = Prices
         y = Prices
         df = pd.DataFrame({'x': x, 'y': y})  # creating a sample dataframe
@@ -71,6 +80,8 @@ def create_plot(feature):
                         y=df['y']
                     )
                 ]
+
+
 
 
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
